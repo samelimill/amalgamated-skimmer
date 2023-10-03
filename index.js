aca54565faac043976ef041c6aabee823f763e23
samelimill <samelimill@gmail.com>
2023-10-02 21:05:39 -0700

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// package imports
 const inquirer = require('inquirer');
 const fs = require('fs');
+// importing classes from lib files
 const { Circle, Square, Triangle } = require('./lib/shapes');
 const Text = require('./lib/text.js');
 
+// array for inquirer in init
 const prompts = [
     {
         type: 'input',
@@ -35,8 +38,11 @@ function init () {
     inquirer
         .prompt(prompts)
         .then((res) =>{
+            // variable to be filled in by later switch statement
             var newShape = '';
+            // constant variable for beginning of svg file
             const svgHead = '<svg version="1.1"\nwidth="300" height="200"\nxmlns="http://www.w3.org/2000/svg">\n'
+            // switch fires off matching class
             switch(res.shapes){
                 case 'Circle':
                     newShape = new Circle(res.color);
@@ -47,7 +53,9 @@ function init () {
                 case 'Triangle':
                     newShape = new Triangle(res.color);
             };
+            // text and text color are constructed in new Text object
             const newText = new Text(res.textcolor, res.letters);
+            // Each element of the file is concatenated together, render() method returns the raw string of the object
             const finalSVG = svgHead + newShape.render() + newText.render();
             return finalSVG;
         }) 
